c0c0afacdc48d2ad5f3c6ba27d04977967c8530a
matheusgbraga <55250511+matheusgbraga@users.noreply.github.com>
2019-09-13 14:08:58 -0300

    Add files via upload

---
+++ classeA.java
@@ -0,0 +1,27 @@
+public class A {
+	private int A1;
+	private float A2;
+	
+	public int getA1(){
+		return A1;
+	}
+	public in setA1 (int A1) {
+		this.A1 = A1;
+	}
+	
+	public float getA2(){
+		return A2;
+	}
+	public float setA2 (float A2) {
+		this.A2 = A2;
+	}
+}
+
+
+
+
+
+
+
+
+
